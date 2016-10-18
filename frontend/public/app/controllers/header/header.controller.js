87cc31628b8f0b6a6b26dd9ff5c52c95d33d9a3d
Armindo Carvalho <arcarvalho@deloitte.pt>
2016-10-18 19:17:01 +0100

    Commit of list of contacts

--- frontend/public/app/controllers/header/header.controller.js
+++ frontend/public/app/controllers/header/header.controller.js
@@ -12,14 +12,14 @@ app.controller('HeaderController', ['$scope', 'ContactsService', function($scope
         console.log(errorCode);
         $scope.IsLoading = false;
     }
-
+    
     //Start Function
     $scope.Start = function() {
         //load Persons
-      /*  $scope.IsLoading = true;
+       $scope.IsLoading = true;
         setTimeout(function() {
             ContactsService.GetAll($scope.OnCompleteGetAll, $scope.OnErrorGetAll);
-        }, 1000);*/
+        }, 1000);
 
     };
     $scope.Start();
--- frontend/public/app/directives/contact-card/contact-card.directive.js
+++ frontend/public/app/directives/contact-card/contact-card.directive.js
@@ -2,7 +2,8 @@ app.directive('contactCard', ["ContactsService", function(ContactsService) {
   return {
     restrict: 'E', //E = element, A = attribute, C = class, M = comment
     scope: {
-      personid: "="
+      namecontact: "=",
+      phonenumber:  "="
     },
     templateUrl: 'app/directives/contact-card/contact-card.directive.html',
     link: function($scope, element, attrs) {
