b119887489f5ebd85f226bb0181f792cbaedb745
Armindo Carvalho <arcarvalho@deloitte.pt>
2016-10-19 18:00:10 +0100

    Some functionalities added in css

--- frontend/public/app/directives/contact-card/contact-card.directive.js
+++ frontend/public/app/directives/contact-card/contact-card.directive.js
@@ -1,20 +1,23 @@
-app.directive('contactCard', ["ContactsService","ActiveContactService", function(ContactsService, ActiveContactService) {
-  return {
-    restrict: 'E', //E = element, A = attribute, C = class, M = comment
-    scope: {
-      namecontact: "=",
-      phonenumber:  "=",
-      contactid: "="
-    },
-    templateUrl: 'app/directives/contact-card/contact-card.directive.html',
-    link: function($scope, element, attrs) {
-      //init card
-      $scope.showUser = function(contactId){
-        ActiveContactService.changeActiveContact(contactId, function(){
+app.directive('contactCard', ["ContactsService", "ActiveContactService", function(ContactsService, ActiveContactService) {
+    return {
+        restrict: 'E', //E = element, A = attribute, C = class, M = comment
+        scope: {
+            namecontact: "=",
+            phonenumber: "=",
+            contactid: "="
+        },
+        templateUrl: 'app/directives/contact-card/contact-card.directive.html',
+        link: function($scope, element, attrs) {
+            //init card
+            $scope.showUser = function(contactId) {
 
-        });
+                //get the last active contact element and set disable mode
+               document.getElementById(ActiveContactService.ActiveContact.id).className = "contact-card-div-no-active";
+               console.log(ActiveContactService.ActiveContact.id);
+               ActiveContactService.changeActiveContact(contactId);
+               document.getElementById(contactId).className = "contact-card-div-active";
 
-      }
-    }
-  };
+            }
+        }
+    };
 }]);
--- frontend/public/app/services/active-contact/active-contact.service.js
+++ frontend/public/app/services/active-contact/active-contact.service.js
@@ -3,12 +3,12 @@ app.service('ActiveContactService', ['ContactsService', function(ContactsService
     this.ActiveContact = {value:null} ;
     var thisService = this;
 
-    this.changeActiveContact = function(contactId, onComplete){
+    this.changeActiveContact = function(contactId){
       ContactsService.GetById(contactId,
          function(onSuccess)
       {
         thisService.ActiveContact.value =  onSuccess;
-        onComplete();
+
       },
        function(onError){
         console.log(onError);
