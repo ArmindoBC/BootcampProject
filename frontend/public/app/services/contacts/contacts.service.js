cddad2f03ea2301b6948455a69e2b1b92eae1b70
Armindo Carvalho <arcarvalho@deloitte.pt>
2016-10-19 20:02:40 +0100

    Search functionality done

--- frontend/public/app/services/contacts/contacts.service.js
+++ frontend/public/app/services/contacts/contacts.service.js
@@ -89,6 +89,24 @@ app.service('ContactsService', ['UtilsService', '$http', function(UtilsService, 
         });
     }
 
+    this.GetAutocomplete = function(string, onComplete, onError) {
+      var thisHandler = this;
+
+        $http({
+            method: 'GET',
+            url: 'http://localhost:9003/contact/autocomplete/' + string
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            thisHandler.AllItems = response.data;
+            onComplete(response.data);
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            onError(response);
+        });
+    }
+
 
 
 }]);
